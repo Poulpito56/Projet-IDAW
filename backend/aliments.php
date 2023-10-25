714c88e81dfa5081e65173f468424cc0e6479815
Pompiquoi <camjuman@gmail.com>
2023-10-25 11:44:04 +0200

    majuscule nom colonne bdd requètes sql

--- backend/aliments.php
+++ backend/aliments.php
@@ -6,7 +6,7 @@
     switch ($_SERVER['REQUEST_METHOD']){
 
         case 'GET':
-            $request = $pdo->prepare("SELECT * FROM aliment ORDER BY id_aliment ASC");
+            $request = $pdo->prepare("SELECT * FROM ALIMENT ORDER BY ID_ALIMENT ASC");
 
             if(!$request->execute()){
                 http_response_code(500);
@@ -29,12 +29,12 @@
                 echo json_encode(["message" => "Les champs 'Code barre', 'Nom', 'Type' et 'Régime alimentaire' sont obligatoires."]);
             } else {
                 if($data -> type == 0){
-                    $request = $pdo->prepare("INSERT INTO aliment (id_aliment, id_regime, nom, image_url, type, bicarbonate, calcium, chlorure, fluor, magnesium, nitrate, potassium, silice, sodium, sulfate)
+                    $request = $pdo->prepare("INSERT INTO ALIMENT (ID_ALIMENT, ID_REGIME, NOM, IMAGE_URL, TYPE, BICARBONATE, CALCIUM, CHLORURE, FLUOR, MAGNESIUM, NITRATE, POTASSIUM, SILICE, SODIUM, SULFATE)
                         VALUES ('{$data->id_aliment}', '{$data->id_regime}', '{$data->nom}', '{$data->image_url}', '{$data->type}', 
                         '{$data->bicarbonate}', '{$data->calcium}', '{$data->chlorure}', '{$data->fluor}', '{$data->magnesium}', '{$data->nitrate}', '{$data->potassium}', '{$data->silice}', '{$data->sodium}', '{$data->sulfate}')
                     ");
                 } else {
-                    $request = $pdo->prepare("INSERT INTO aliment (id_aliment, id_regime, nom, image_url, type, glucide, energie, gras, fibre, proteine, sel, graisses_saturees, sucre) 
+                    $request = $pdo->prepare("INSERT INTO ALIMENT (ID_ALIMENT, ID_REGIME, NOM, IMAGE_URL, TYPE, GLUCIDE, ENERGIE, GRAS, FIBRE, PROTEINE, SEL, GRAISSES_SATUREES, SUCRE) 
                         VALUES ('{$data->id_aliment}', '{$data->id_regime}', '{$data->nom}', '{$data->image_url}', '{$data->type}', 
                         '{$data->glucide}'), '{$data->energie}'), '{$data->gras}'), '{$data->fibre}'), '{$data->proteine}'), '{$data->sel}'), '{$data->graisses_saturees}'), '{$data->sucre}')
                     ");
@@ -58,11 +58,11 @@
                 http_response_code(400);
                 echo json_encode(["message" => "Le champ 'Code barre' est obligatoire."]);
             } else {
-                $request = $pdo->prepare("DELETE FROM aliment WHERE id_aliment = {$data->id_aliment}");
+                $request = $pdo->prepare("DELETE FROM ALIMENT WHERE ID_ALIMENT = {$data->id_aliment}");
             
                 if(!$request->execute()){
                     http_response_code(500);
-                    echo json_encode(["message" => "erreur lors de la suppression de l'aliment."]);
+                    echo json_encode(["message" => "Erreur lors de la suppression de l'aliment."]);
                 }else{
                     // retrieve data from database using fetch(PDO::FETCH_OBJ) and
                     http_response_code(204);
@@ -82,100 +82,96 @@
                 $updates = [];
         
                 if (isset($data->id_regime)) {
-                    $updates[] = "id_regime = '{$data->id_regime}'";
+                    $updates[] = "ID_REGIME = '{$data->id_regime}'";
                 }
                 
                 if (isset($data->nom)) {
-                    $updates[] = "nom = '{$data->nom}'";
+                    $updates[] = "NOM = '{$data->nom}'";
                 }
                 
                 if (isset($data->image_url)) {
-                    $updates[] = "image_url = '{$data->image_url}'";
+                    $updates[] = "IMAGE_URL = '{$data->image_url}'";
                 }
                 
                 if (isset($data->type)) {
-                    $updates[] = "type = '{$data->type}'";
+                    $updates[] = "TYPE = '{$data->type}'";
                 }
                 
                 if (isset($data->glucide)) {
-                    $updates[] = "glucide = '{$data->glucide}'";
+                    $updates[] = "GLUCIDE = '{$data->glucide}'";
                 }
                 
                 if (isset($data->energie)) {
-                    $updates[] = "energie = '{$data->energie}'";
+                    $updates[] = "ENERGIE = '{$data->energie}'";
                 }
                 
                 if (isset($data->gras)) {
-                    $updates[] = "gras = '{$data->gras}'";
+                    $updates[] = "GRAS = '{$data->gras}'";
                 }
                 
                 if (isset($data->fibre)) {
-                    $updates[] = "fibre = '{$data->fibre}'";
+                    $updates[] = "FIBRE = '{$data->fibre}'";
                 }
                 
                 if (isset($data->proteine)) {
-                    $updates[] = "proteine = '{$data->proteine}'";
+                    $updates[] = "PROTEINE = '{$data->proteine}'";
                 }
                 
                 if (isset($data->sel)) {
-                    $updates[] = "sel = '{$data->sel}'";
+                    $updates[] = "SEL = '{$data->sel}'";
                 }
                 
                 if (isset($data->graisses_saturees)) {
-                    $updates[] = "graisses_saturees = '{$data->graisses_saturees}'";
+                    $updates[] = "GRAISSES_SATUREES = '{$data->graisses_saturees}'";
                 }
                 
                 if (isset($data->sucre)) {
-                    $updates[] = "sucre = '{$data->sucre}'";
+                    $updates[] = "SUCRE = '{$data->sucre}'";
                 }
                 
                 if (isset($data->bicarbonate)) {
-                    $updates[] = "bicarbonate = '{$data->bicarbonate}'";
+                    $updates[] = "BICARBONATE = '{$data->bicarbonate}'";
                 }
                 
                 if (isset($data->calcium)) {
-                    $updates[] = "calcium = '{$data->calcium}'";
+                    $updates[] = "CALCIUM = '{$data->calcium}'";
                 }
                 
                 if (isset($data->chlorure)) {
-                    $updates[] = "chlorure = '{$data->chlorure}'";
+                    $updates[] = "CHLORURE = '{$data->chlorure}'";
                 }
                 
                 if (isset($data->fluor)) {
-                    $updates[] = "fluor = '{$data->fluor}'";
+                    $updates[] = "FLUOR = '{$data->fluor}'";
                 }
                 
                 if (isset($data->magnesium)) {
-                    $updates[] = "magnesium = '{$data->magnesium}'";
+                    $updates[] = "MAGNESIUM = '{$data->magnesium}'";
                 }
                 
                 if (isset($data->nitrate)) {
-                    $updates[] = "nitrate = '{$data->nitrate}'";
-                }
-                
-                if (isset($data->nitrate)) {
-                    $updates[] = "nitrate = '{$data->nitrate}'";
+                    $updates[] = "NITRATE = '{$data->nitrate}'";
                 }
                 
                 if (isset($data->potassium)) {
-                    $updates[] = "potassium = '{$data->potassium}'";
+                    $updates[] = "POTASSIUM = '{$data->potassium}'";
                 }
                 
                 if (isset($data->silice)) {
-                    $updates[] = "silice = '{$data->silice}'";
+                    $updates[] = "SILICE = '{$data->silice}'";
                 }
                 
                 if (isset($data->sodium)) {
-                    $updates[] = "sodium = '{$data->sodium}'";
+                    $updates[] = "SODIUM = '{$data->sodium}'";
                 }
                 
                 if (isset($data->sulfate)) {
-                    $updates[] = "sulfate = '{$data->sulfate}'";
+                    $updates[] = "SULFATE = '{$data->sulfate}'";
                 }
 
                 if (!empty($updates)) {
                     // Construire la requête SQL UPDATE avec les champs à mettre à jour
-                    $sql = "UPDATE aliment SET " . implode(', ', $updates) . " WHERE id_aliment = {$data->id_aliment}";
+                    $sql = "UPDATE ALIMENT SET " . implode(', ', $updates) . " WHERE ID_ALIMENT = {$data->id_aliment}";
                     
                     $request = $pdo->prepare($sql);
         
