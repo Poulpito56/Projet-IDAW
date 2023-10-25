d1cf11f6c479560d1f4c471e5ba163bd2ffbcef3
Evan DENIS <evan.denis@etu.imt-nord-europe.fr>
2023-10-25 09:57:57 +0200

    add JSON datas ISSUE

--- backend/init_db.php
+++ backend/init_db.php
@@ -16,4 +16,37 @@ try {
   echo "Erreur : " . $e->getMessage();
 }
 
+// URL de l'API open food facts
+$url = 'https://world.openfoodfacts.org/cgi/search.pl?action=process&sort_by=random&page_size=2&json=1&fields=nutriments,ingredients,id,product_name,image_url,categories_tags';
+
+// Utilisation de cURL pour effectuer la requête GET
+$curl = curl_init($url);
+
+// Configuration de cURL
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+// Exécution de la requête
+$response = curl_exec($curl);
+
+// Vérification des erreurs
+if ($response === false) {
+  echo 'Erreur de cURL : ' . curl_error($curl);
+  die();
+}
+
+// Fermeture de la session cURL
+curl_close($curl);
+
+// Conversion de la réponse JSON en un tableau associatif PHP
+$data = json_decode($response, true);
+
+if ($data === null) {
+  echo 'Erreur de conversion en JSON : ' . json_last_error_msg();
+} else {
+  // Maintenant, "$data" contient le tableau JSON récupéré depuis l'API
+  print_r($data);
+
+  // Vous pouvez traiter les données ici
+}
+
 $pdo = NULL;
