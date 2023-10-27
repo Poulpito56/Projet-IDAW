925b4fe4210d0af7404ab541a3c2fffdf1de5d73
Pompiquoi <camjuman@gmail.com>
2023-10-27 17:21:03 +0200

    datatables avec code barre + roboto font

--- frontend/aliments.php
+++ frontend/aliments.php
@@ -7,6 +7,11 @@
         <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
         <script src="//cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="//cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+        <link rel="stylesheet" href="css/styles.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Libre+Barcode+EAN13+Text&family=Roboto&display=swap" rel="stylesheet">
+
     </header>
 
     <body>
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -7,8 +7,16 @@ function afficherAliments(){
                   },
                   columns: [
                         { data: 'NOM'},
-                        { data: 'ID_ALIMENT'},
-                        { data: 'ENERGIE'},
+                        { data: 'ID_ALIMENT',
+                        render: function(data){
+                              return '<div class="code-barre">'+data+'</div>'}},
+                        { data: 'ENERGIE',
+                        render: function(data){var calorie = DataTable.render
+                              .number(' ', ',', 1, '', 'kcal')
+                              .display(data);
+                              return calorie;
+                              }
+                        },
                         { data: 'IMAGE_URL',
                         render: function(data){ return '<img src="'+data+'" style="height:70px;"/>'}}
                         
