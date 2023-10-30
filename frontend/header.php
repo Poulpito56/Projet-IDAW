df9609864128261c232566e9b48523733e49222c
Evan DENIS <evan.denis@etu.imt-nord-europe.fr>
2023-10-30 12:43:40 +0100

    add header

---
+++ frontend/header.php
@@ -0,0 +1,8 @@
+<header>
+  <img class="logo-site" src="imgs/logos/i_manger_mieux.png" alt="logo_site">
+  <div class="navbar">
+    <img class="nav-img" src="imgs/logos/home.png" alt="home">
+    <img class="nav-img" src="imgs/logos/journal.png" alt="journal">
+    <img class="nav-img" src="imgs/logos/profil.png" alt="profil">
+  </div>
+</header>
--- frontend/index.php
+++ frontend/index.php
@@ -1,9 +1,17 @@
 <!DOCTYPE html>
 <html>
 
-<head></head>
+<head>
+  <link rel="stylesheet" href="css/header.css">
+  <link rel="stylesheet" href="css/styles.css">
+</head>
 
 <body>
+  <?php
+  session_start();
+  require_once('header.php');
+  ?>
+  <div style="height: 2000px;"> vhze</div>
   HAHAHAHAHAHAHAHAAHAHAHAHA tu es connecté
   <a href="connexion.php">REVIEEEEEEENS</a>
 </body>
