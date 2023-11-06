c6c40e47deab2b125a18656c6aaa4b3a08b46329
Pompiquoi <camjuman@gmail.com>
2023-11-06 18:03:08 +0100

    api modifiée pour plat

--- frontend/pages/plat.php
+++ frontend/pages/plat.php
@@ -2,3 +2,9 @@
     Ajouter un plat
 </h2>
 
+<form method="POST">
+  <button type="submit" class="nav-button">
+    <input type="hidden" name="page" value="ajout_plat">
+    <div class="tooltip"><?php echo $tra->add_dish; ?></div>
+  </button>
+</form>
