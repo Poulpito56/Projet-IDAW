c7fb4a9f6a2a077719e69f8cea957c07eb2fb125
Pompiquoi <camjuman@gmail.com>
2023-11-07 22:37:57 +0100

    inverse drapeau footer

--- frontend/pages/footer.php
+++ frontend/pages/footer.php
@@ -14,7 +14,7 @@ function change_lang()
   <form method="GET" class="lang-form">
     <button type="submit" class="lang-button">
       <input type="hidden" name="lang" value="<?php echo change_lang(); ?>">
-      <img class="lang-img" src="imgs/lang/<?php echo $_SESSION['lang']; ?>.png" alt="traduction">
+      <img class="lang-img" src="imgs/lang/<?php echo change_lang(); ?>.png" alt="traduction">
       <div class="tooltip"><?php echo $tra->language; ?></div>
     </button>
   </form>
