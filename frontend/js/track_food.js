d9d0633e601b51b10a90e4946e9a74babaeba686
Evan DENIS <evan.denis@etu.imt-nord-europe.fr>
2023-11-09 12:10:29 +0100

    update track_food

--- frontend/js/track_food.js
+++ frontend/js/track_food.js
@@ -1,21 +1,23 @@
 const request = new XMLHttpRequest();
 
-function formatDate(date) {
-  var year = date.toLocaleString("default", { year: "numeric" });
-  var month = date.toLocaleString("default", { month: "2-digit" });
-  var day = date.toLocaleString("default", { day: "2-digit" });
-
-  return [year, month, day].join('-');
-}
-
 request.onreadystatechange = function () {
   if (request.readyState == 4) {
     if (request.status !== 200) {
       mess = document.getElementById('track_food_error_message');
-      mess.innerHTML = JSON.parse(request.response).message
+      mess.innerHTML = reverse(JSON.parse(request.response).message)
     } else {
       var trackFoodContainer = document.getElementById('track_food_container');
-      console.log(request.response)
+      trackFoodContainer.innerHTML = "";
+      const rep = JSON.parse(request.response);
+      for (consommation in rep) {
+        let row = document.createElement('div');
+        for (field in rep[consommation]) {
+          let displayField = document.createElement('div');
+          displayField.innerHTML = field + " : " + rep[consommation][field];
+          row.append(displayField);
+        }
+        trackFoodContainer.append(row);
+      }
     }
   }
 };
@@ -25,7 +27,7 @@ dateSelection = document.getElementById('date_selection');
 request.open("GET", `http://localhost/Projet%20IDAW/backend/consommer.php?login=${user}&date_consommation=${dateSelection.value}`, true);
 request.send()
 
-dateSelection.addEventListener("submit", function (event) {
-  event.preventDefault();
-  connexion();
-})
+dateSelection.addEventListener("change", function () {
+  request.open("GET", `http://localhost/Projet%20IDAW/backend/consommer.php?login=${user}&date_consommation=${dateSelection.value}`, true);
+  request.send()
+});
