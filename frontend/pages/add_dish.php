67851e35acc5e1cf5ef3d9b6dbeee243e34a3236
Evan DENIS <evan.denis@etu.imt-nord-europe.fr>
2023-11-09 11:30:43 +0100

    add date_selection

--- frontend/pages/add_dish.php
+++ frontend/pages/add_dish.php
@@ -1,26 +1,26 @@
 <script src="js/main.js" defer></script>
 <form id="nouvPlatForm" method="PUT">
   <div class="custom-table-field">
-    <h2 class="field-name"><?php echo ($tra->titleDish)." :"; ?></h2>
-    <input class="text-input" type="text" id="titreNouvPlat" pattern="[A-Za-z0-9]" require>
+    <h2 class="field-name"><?php echo ($tra->titleDish) . " :"; ?></h2>
+    <input class="text-input" type="text" id="titreNouvPlat" pattern="[A-Za-z0-9 ]+" require>
   </div>
-  <h3 class="field-name"><?php echo ($tra->food)." :"; ?></h3>
+  <h3 class="field-name"><?php echo ($tra->food) . " :"; ?></h3>
   <table id="alimentTablePlat" class="display nowrap" style="width:100%">
     <thead>
-        <tr>
-            <th scope="col">Nom</th>
-            <th scope="col">Code barre</th>
-            <th scope="col">Energie</th>
-            <th scope="col">Image</th>
-        </tr>
+      <tr>
+        <th scope="col">Nom</th>
+        <th scope="col">Code barre</th>
+        <th scope="col">Energie</th>
+        <th scope="col">Image</th>
+      </tr>
     </thead>
     <tbody>
     </tbody>
   </table>
 
   <tr>
-      <td id="validate-button-dish"><input class="text-button" type="submit" value="<?php echo $tra->validate; ?>" /></td>
-    </tr>
+    <td id="validate-button-dish"><input class="text-button" type="submit" value="<?php echo $tra->validate; ?>" /></td>
+  </tr>
 </form>
 
 <script>
@@ -34,7 +34,7 @@
     .then(data => {
       var plat_info = data[0];
       afficherAlimentsPlat(plat_info.ID_ALIMENT);
-      document.getElementById('titreNouvPlat').value = (plat_info.NOM == "Plat n°")  ? plat_info.NOM + plat_info.ID_ALIMENT : plat_info.NOM;
+      document.getElementById('titreNouvPlat').value = (plat_info.NOM == "Plat n°") ? plat_info.NOM + plat_info.ID_ALIMENT : plat_info.NOM;
     })
     .catch(error => {
       console.error('Erreur :', error);
---
+++ frontend/pages/date_selection.php
@@ -0,0 +1,10 @@
+<?php
+$yesterday  = mktime(0, 0, 0, date("m"), date("d") - 1, date("Y"));
+$lestWeek  = mktime(0, 0, 0, date("m"), date("d") - 7, date("Y"));
+$lastMonth = mktime(0, 0, 0, date("m") - 1, date("d"),   date("Y"));
+?>
+<select class="text-input" id='date_selection'>
+  <option value="<?php echo date('Y-m-d', $yesterday); ?>"><?php echo $tra->last_day; ?></option>
+  <option value="<?php echo date('Y-m-d', $lestWeek); ?>"><?php echo $tra->last_week; ?></option>
+  <option value="<?php echo date('Y-m-d', $lastMonth); ?>"><?php echo $tra->last_month; ?></option>
+</select>
