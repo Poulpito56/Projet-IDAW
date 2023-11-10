a7a1486cb46dc19b49881a9386152d8d3a94ae5b
Evan DENIS <evan.denis@etu.imt-nord-europe.fr>
2023-11-10 12:03:21 +0100

    update track_food

--- frontend/js/track_food.js
+++ frontend/js/track_food.js
@@ -1,23 +1,82 @@
 const request = new XMLHttpRequest();
 
+function modifyConso(id_conso) {
+
+}
+
+async function displayConso(consommation) {
+  id_aliment = consommation['ID_ALIMENT'];
+  return fetch(`http://localhost/Projet%20IDAW/backend/aliment.php?id_aliment=${id_aliment}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Erreur lors de la requête : ${response.status} - ${response.statusText}`);
+      }
+      return response.json(); // Convertir la réponse en JSON
+    })
+    .then(response => { return response[0] })
+    .then(aliment => {
+
+      // Ajout image aliment
+      let aliment_image = document.createElement('img');
+      aliment_image.classList.add('consomation-aliment-image');
+      aliment_image.setAttribute('src', aliment['IMAGE_URL']);
+
+      // Ajout nom aliment
+      let aliment_name = document.createElement('div');
+      aliment_name.classList.add('consomation-aliment-name');
+      aliment_name.innerHTML = aliment['NOM'];
+
+      // Ajout quantite aliment
+      let quantite = document.createElement('div');
+      quantite.classList.add('consomation-aliment-quantite');
+      quantite.innerHTML = consommation['QUANTITE'];
+
+      // Ajout bouton modifier
+      let modifier = document.createElement('button');
+      modifier.classList.add('consomation-aliment-modifier');
+      modifier.classList.add('text-button');
+      modifier.setAttribute('onclick', `modifyConso(${consommation['ID_CONSOMMATION']})`);
+
+      return { aliment_image, aliment_name, quantite, modifier };
+    })
+    .catch(error => {
+      console.error('Erreur :', error);
+    });
+}
+
 request.onreadystatechange = function () {
   if (request.readyState == 4) {
     if (request.status !== 200) {
       mess = document.getElementById('track_food_error_message');
       mess.innerHTML = reverse(JSON.parse(request.response).message)
     } else {
+      // Construction du code HTML de la page pour afficher les consommations
       var trackFoodContainer = document.getElementById('track_food_container');
       trackFoodContainer.innerHTML = "";
-      const rep = JSON.parse(request.response);
-      for (consommation in rep) {
+      var rep = JSON.parse(request.response).reverse();
+
+      for (var key in rep) { // Parcours des éléments de la table consommation demandés
         let row = document.createElement('div');
-        for (field in rep[consommation]) {
-          let displayField = document.createElement('div');
-          displayField.innerHTML = field + " : " + rep[consommation][field];
-          row.append(displayField);
-        }
-        trackFoodContainer.append(row);
+        row.classList.add('consomation-aliment-row');
+        consommation = rep[key];
+        row.id = "id_consommation_" + consommation['ID_CONSOMMATION'];
+        delete consommation['ID_CONSOMMATION'];
+
+        const fields = displayConso(consommation)
+
+        fields.then(data => {
+
+          for (let field in data) {
+            row.append(data[field]);
+          }
+
+          trackFoodContainer.append(row);
+
+          displayModify();
+
+        })
       }
+
     }
   }
 };
--- frontend/pages/track_food.php
+++ frontend/pages/track_food.php
@@ -1,5 +1,13 @@
 <script>
   var user = <?php echo json_encode($_SESSION['utilisateur']); ?>;
+
+  function displayModify() {
+    var elements = document.getElementsByClassName('consomation-aliment-modifier');
+
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].innerHTML = "<?php echo $tra->modify; ?>";
+    }
+  }
 </script>
 <script src="js/track_food.js" defer></script>
 <div id="track_food_container">
