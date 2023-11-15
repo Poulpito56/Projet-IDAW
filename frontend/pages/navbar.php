b0b7153e024e458f1bc07aa19731a51bb4d26230
Pompiquoi <camjuman@gmail.com>
2023-11-15 16:10:37 +0100

    update navbar css

--- frontend/pages/navbar.php
+++ frontend/pages/navbar.php
@@ -5,35 +5,35 @@
   }
   ?>
   <form method="GET">
-    <button id="navbar_button_page_journal" type="submit" class="nav-button">
+    <button id="navbar_button_page_journal" type="submit" class="nav-button inactive-nav-button">
       <input type="hidden" name="page" value="journal">
       <img class="nav-img" src="imgs/logos/journal.png" alt="<?php echo $tra->journal; ?>">
       <div class="tooltip"><?php echo $tra->journal; ?></div>
     </button>
   </form>
   <form method="GET">
-    <button id="navbar_button_page_track_food" type="submit" class="nav-button">
+    <button id="navbar_button_page_track_food" type="submit" class="nav-button inactive-nav-button">
       <input type="hidden" name="page" value="track_food">
       <img class="nav-img" src="imgs/logos/track_food.png" alt="<?php echo $tra->track_food; ?>">
       <div class="tooltip"><?php echo $tra->track_food; ?></div>
     </button>
   </form>
   <form method="GET">
-    <button id="navbar_button_page_profile" type="submit" class="nav-button">
+    <button id="navbar_button_page_profile" type="submit" class="nav-button inactive-nav-button">
       <input type="hidden" name="page" value="profile">
       <img class="nav-img" src="imgs/logos/profil.png" alt="<?php echo $tra->profile; ?>">
       <div class="tooltip"><?php echo $tra->profile; ?></div>
     </button>
   </form>
   <form method="GET">
-    <button id="navbar_button_page_dish" type="submit" class="nav-button">
+    <button id="navbar_button_page_dish" type="submit" class="nav-button inactive-nav-button">
       <input type="hidden" name="page" value="dish">
       <img class="nav-img" src="imgs/logos/aliments.png" alt="<?php echo $tra->dish; ?>">
       <div class="tooltip"><?php echo $tra->dish; ?></div>
     </button>
   </form>
   <form method="GET">
-    <button type="submit" class="nav-button">
+    <button type="submit" class="nav-button inactive-nav-button">
       <input type="hidden" name="log_out">
       <img class="nav-img" src="imgs/logos/deconnection.png" alt="<?php echo $tra->log_out; ?>">
       <div class="tooltip"><?php echo $tra->log_out; ?></div>
@@ -46,6 +46,6 @@
   // Vérifier si l'élément avec l'ID existe
   if (actualPage) {
     // Ajouter la classe à l'élément
-    actualPage.classList.add("active-nav-button");
+    actualPage.classList.remove("inactive-nav-button");
   }
 </script>
